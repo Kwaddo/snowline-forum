cfdb38ab0849cb4a2af732c2c1dbba5f9867d7ce
mibrahim <moatazusama9@gmail.com>
2024-11-06 17:28:59 +0300

    comment popup

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -275,7 +275,7 @@ func (app *app) ProfilePageHandler(w http.ResponseWriter, r *http.Request) {
 				return
 			}
 
-			tmp, err := template.ParseFiles("./assets/templates/profilepage.html")
+			tmp, err := template.ParseFiles("./assets/templates/profile.html")
 			if err != nil {
 				ErrorHandle(w, 500, "Internal Server Error")
 				log.Println(err)
